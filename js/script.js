44e31312c0e34154fc9ce6086cc5c911ef5cc581
Theo <tatang@sfu.ca>
2020-12-01 16:03:46 -0800

    Updated Sliders and Support Section

--- js/script.js
+++ js/script.js
@@ -8,3 +8,35 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+var supportBtn = document.getElementById("support-us-btn");
+var closeBtn = document.getElementById("close-support-btn");
+
+supportBtn.addEventListener('click', showSupportSection);
+closeBtn.addEventListener('click', hideSupportSection);
+
+function showSupportSection() {
+    var section = document.getElementById("support-section");
+    var sliderChoose = document.getElementById("outerChoose");
+    var sliderFeature = document.getElementById("outerFeature");
+    var sliderDecision = document.getElementById("outerDecision");
+
+    sliderChoose.style.width = "55vw";
+    sliderFeature.style.width = "55vw";
+    sliderDecision.style.width = "55vw";
+
+    section.classList.remove("content-hidden");
+}
+
+function hideSupportSection() {
+    var section = document.getElementById("support-section");
+    var sliderChoose = document.getElementById("outerChoose");
+    var sliderFeature = document.getElementById("outerFeature");
+    var sliderDecision = document.getElementById("outerDecision");
+
+    sliderChoose.style.width = "80vw";
+    sliderFeature.style.width = "80vw";
+    sliderDecision.style.width = "80vw";
+
+    section.classList.add("content-hidden");
+}
